4b16e5894008b6e93cf5872d6ea8dca40251492d
Rommel Lasso <soccerply@msn.com>
2017-12-19 00:14:18 -0500

    Initial Files

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS quizzes;
+DROP TABLE IF EXISTS grades;
+
+CREATE TABLE student (
+  Identifier INTEGER PRIMARY KEY,
+  FirstName TEXT,
+  LastName TEXT
+);
+
+CREATE TABLE quizzes (
+  Identifier INTEGER PRIMARY KEY,
+  Subject TEXT,
+  TotalQuestions INTEGER,
+  RawDate TEXT
+);
+
+CREATE TABLE grades (
+  Identifier INTEGER PRIMARY KEY,
+  Student INTEGER,
+  Quiz INTEGER,
+  Grade INTEGER,
+  FOREIGN KEY (Student) REFERENCES student(Identifier),
+  FOREIGN KEY (Quiz) REFERENCES quizzes(Identifier)
+);
