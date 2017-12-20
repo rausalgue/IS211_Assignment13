e18ae1929e156781f3d5872ccd0391dd9899daa4
Rommel Lasso <soccerply@msn.com>
2017-12-20 02:16:32 -0500

    Added student data and quiz data

--- Blackboard.py
+++ Blackboard.py
@@ -2,27 +2,57 @@
 # -*- coding: utf-8 -*-
 """Assignemnt Week 13 - Flask App"""
 
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
 import re
 import sqlite3 as lite
+from contextlib import closing
 
-con = lite.connect('hw13.db')
-con.row_factory = lite.Row
-cursor = con.cursor()
-
-def init_db():
-    with open('schema.sql') as db:
-        cursor.executescript(db.read())
-
-    temp_student = (301101,'John', 'Smith')
-    temp_quiz = (201, 'Python Basics', 5, '02-05-2015')
-    temp_grade = (1, 301101, 201, 85)
-
-    cursor.execute('INSERT into student(Identifier, FirstName, LastName) VALUES (?,?,?)', temp_student)
-    cursor.execute('INSERT into quizzes(Identifier, Subject,TotalQuestions, Date) VALUES (?,?,?,?)', temp_quiz)
-    cursor.execute('INSERT into grades(Identifier, Student, Quiz, Grade) VALUES (?,?,?)', temp_grade)
+DATABASE = 'hw13.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'password'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+con = lite.connect('hw13.db')
+
+cur = con.cursor()
+
+cur.executescript("""
+    DROP TABLE IF EXISTS student;
+    DROP TABLE IF EXISTS quizzes;
+    DROP TABLE IF EXISTS grades;
+    CREATE TABLE student(Identifier INTEGER PRIMARY KEY, FirstName TEXT, LastName TEXT);
+    CREATE TABLE quizzes(Identifier INTEGER PRIMARY KEY, Subject TEXT, TotalQuestions INTEGER, Date TEXT);
+    CREATE TABLE grades(Identifier INTEGER PRIMARY KEY, Student INTEGER, Quiz INTEGER, Grade INTEGER, FOREIGN KEY (Student) REFERENCES student(Identifier), FOREIGN KEY (Quiz) REFERENCES quizzes(Identifier));
+    """)
+
+cur.execute('INSERT into student VALUES (1201,"John","Smith");')
+cur.execute('INSERT into quizzes VALUES (101,"Python Basics",5,"12-05-2015");')
+cur.execute('INSERT into grades VALUES (1,1201,101,85);')
+
+cur.execute('INSERT into student VALUES (1202,"Rommel","Lasso");')
+cur.execute('INSERT into grades VALUES (2,1202,101,100);')
+
+con.commit()
+
+def connect_db():
+    return lite.connect(app.config['DATABASE'])
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+#app = Flask(__name__)
 app.secret_key = 'lasso91'
 
 def getSessionInfo ():
@@ -40,8 +70,18 @@ def index():
 def deashboard():
     valid = getSessionInfo()
 
+    student_data = {}
+
+    conns = g.db.execute('select Identifier, FirstName, LastName from student')
+    students = [dict(StudentID=row[0], FirstName=row[1], LastName=row[2])
+               for row in conns.fetchall()]
+
+    conns = g.db.execute('select Identifier, Subject, TotalQuestions, Date from quizzes')
+    quizzes = [dict(QuizId=row[0], Subject=row[1], TotalQuestions=row[2], Date=row[3])
+                for row in conns.fetchall()]
+
     if valid:
-        return render_template('dashboard.html')
+        return render_template('dashboard.html', students = students, quizzes = quizzes)
     else:
         return redirect(url_for('index'))
 
